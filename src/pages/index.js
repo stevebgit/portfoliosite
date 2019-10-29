adef54f94329e9e04a0ba926c01a033199801ec7
Steve Bailey <steveb428@gmail.com>
2019-10-29 05:36:14 -0500

    added captions

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,12 +20,12 @@ import full05 from '../assets/images/fulls/moc.png'
 import full06 from '../assets/images/fulls/clear.png'
 
 const DEFAULT_IMAGES = [
-    { id: '1', source: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '2', source: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '3', source: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '4', source: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '5', source: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '6', source: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
+    { id: '1', source: full01, thumbnail: thumb01, caption: 'San Diego, CA', description: 'SQL-heavy project, with a lot of legacy OO-based PHP'},
+    { id: '2', source: full02, thumbnail: thumb02, caption: 'Omaha, NE', description: 'Heavy on Laravel and VueJS as well as CSS Desgin.'},
+    { id: '3', source: full03, thumbnail: thumb03, caption: 'Irvine, CA', description: 'Laravel project.'},
+    { id: '4', source: full04, thumbnail: thumb04, caption: 'Santa Monica, CA', description: 'OO PHP and Html5/CSS.'},
+    { id: '5', source: full05, thumbnail: thumb05, caption: 'Burbank, CA', description: 'Created Enterprise-size CMS for NBC Universal using Laravel'},
+    { id: '6', source: full06, thumbnail: thumb06, caption: 'Kirkland, CA', description: "Zend Framework, as well as consuming API's to create a front-end eCommerce U.I."}
 ];
 
 class HomeIndex extends React.Component {
